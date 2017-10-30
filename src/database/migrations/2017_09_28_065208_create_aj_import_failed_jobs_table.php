30aac0e2097242acbba7a0601a853f38b1eb40f3
Parag <parag@ajency.in>
2017-10-30 16:53:30 +0530

    class update

--- src/database/migrations/2017_09_28_065208_create_aj_import_failed_jobs_table.php
+++ src/database/migrations/2017_09_28_065208_create_aj_import_failed_jobs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateFailedJobsTable extends Migration
+class CreateAjImportFailedJobsTable extends Migration
 {
     /**
      * Run the migrations.
